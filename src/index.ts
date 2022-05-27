365bc64c7f8c599fa7779281287f58d17f95a384
Luis Beu <me@luisbeu.de>
2022-05-27 15:34:00 +0200

    add shebang which i had at somepoint but it got lost...

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 import yargs from 'yargs';
 
 yargs(process.argv.slice(2))
     .scriptName('hook-cli')
     .alias('v', 'version')
-    .commandDir('modules', { extensions: ['ts', 'js'] })
+    .commandDir('modules', { extensions: ['ts', 'js'] }) // ts for dev and js for build
     .demandCommand()
     .alias('h', 'help')
     .help('help')
